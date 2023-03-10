8bc1505a07484cab16cccd906f48bbcf35f4ffc7
mamehata <117148042+mamehata@users.noreply.github.com>
2023-03-10 18:59:25 +0900

    Testの中身を更新
    テックアカデミーカリキュラムLesson 4Chapter 4.3を実施

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,12 +7,13 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //変数の宣言
-        int score;
-        //変数の代入
-        score = 150;
-        //コンソールに変数の中身を表示
-        Debug.Log(score);
+        //変数を初期化する
+        int a = 3;
+        int b = 4;
+        //変数aと変数bの合計値を変数cに代入する
+        int c = a + b;
+        //変数cを表示する
+        Debug.Log(c);
     }
 
     // Update is called once per frame
