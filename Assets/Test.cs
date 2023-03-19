18fd3cc5602c5d4fc1942e6e4f80e246911791e4
mamehata <117148042+mamehata@users.noreply.github.com>
2023-03-19 19:11:03 +0900

    Testの更新
    テックアカデミーカリキュラムLesson 4Chapter 8.3練習

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // 第一引数と第二引数の値を足した値を返す関数
-    int Add (int a, int b)
+    // 第一引数と第二引数と第三引数の値を足した値を返す関数
+    int Add (int a, int b, int c)
     {
-        // 第一引数と第二引数の値を足して、変数cに代入する
-        int c = a + b;
-        // 変数cを呼び出し元の関数に返す
-        return c;
+        // 第一引数と第二引数と第三引数の値を足して、変数dに代入する
+        int d = a + b + c;
+        // 変数dを呼び出し元の関数に返す
+        return d;
     }
     // Start is called before the first frame update
     void Start()
     {
-        // Add関数に「3」と「6」の引数を渡し、返り値をnum変数に代入する
-        int num = Add (3, 6);
+        // Add関数に「3」と「6」と「8」の引数を渡し、返り値をnum変数に代入する
+        int num = Add (3, 6, 8);
         // numを表示する
         Debug.Log(num);
     }
