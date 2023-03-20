dacffa96d30218c96e3dc7050412202ab82cb278
mamehata <117148042+mamehata@users.noreply.github.com>
2023-03-20 19:06:52 +0900

    Testの更新
    テックアカデミーカリキュラムLesson 4Chapter 9.3途中

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,23 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+    private int hp = 100;// 体力
+    private int power = 25;// 攻撃力
+
+    // 攻撃用の関数
+    public void Attack ()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    // 防御用の関数
+    public void Defence (int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+}
 public class Test : MonoBehaviour
 {
-    // 第一引数と第二引数と第三引数の値を足した値を返す関数
-    int Add (int a, int b, int c)
-    {
-        // 第一引数と第二引数と第三引数の値を足して、変数dに代入する
-        int d = a + b + c;
-        // 変数dを呼び出し元の関数に返す
-        return d;
-    }
     // Start is called before the first frame update
     void Start()
     {
-        // Add関数に「3」と「6」と「8」の引数を渡し、返り値をnum変数に代入する
-        int num = Add (3, 6, 8);
-        // numを表示する
-        Debug.Log(num);
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
     }
 
     // Update is called once per frame
