d3fdf53a724f0a81ba0f2184002a2b6c9888dd51
mamehata <117148042+mamehata@users.noreply.github.com>
2023-03-21 20:04:47 +0900

    Testの更新
    Lesson4課題「配列を宣言して出力しよう」

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,42 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss
-{
-    private int hp = 100;// 体力
-    private int power = 25;// 攻撃力
 
-    // 攻撃用の関数
-    public void Attack ()
-    {
-        Debug.Log(this.power + "のダメージを与えた");
-    }
-
-    // 防御用の関数
-    public void Defence (int damage)
-    {
-        Debug.Log(damage + "のダメージを受けた");
-        // 残りhpを減らす
-        this.hp -= damage;
-    }
-}
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // Bossクラスの変数を宣言してインスタンスを代入
-        Boss lastboss = new Boss();
+        int[] array = {50, 20, 80, 70, 100};
 
-        // 攻撃用の関数を呼び出す
-        lastboss.Attack();
-        // 防御用の関数を呼び出す
-        lastboss.Defence(3);
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
 
-        Boss midboss = new Boss();
-
-        midboss.Attack();
-        midboss.Defence(8);
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
     }
 
     // Update is called once per frame
