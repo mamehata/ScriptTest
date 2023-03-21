e56dcc3053a02b2824c7859047f1dbe4502552d1
mamehata <117148042+mamehata@users.noreply.github.com>
2023-03-21 20:38:43 +0900

    Testの更新
    Lesson4発展課題「Bossクラスに、変数mpとMagic関数を作成してStart関数から呼び出そう」

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,22 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+    private int hp = 100;
+    private int power = 25;
+    private int mp = 53;
 
+    public void Attack ()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Defence (int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        this.hp -= damage;
+    }
+
+    public void Magic (int magic)
+    {
+        if (this.mp >= magic)
+        {
+            this.mp -= magic;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため、魔法が使えない。");
+        }
+    }
+}
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        int[] array = {50, 20, 80, 70, 100};
+        Boss lastboss = new Boss();
 
-        for (int i = 0; i < array.Length; i++)
+        lastboss.Attack();
+        lastboss.Defence(3);
+        for (int i = 0; i <= 10; i++)
         {
-            Debug.Log(array[i]);
-        }
-
-        for (int i = array.Length - 1; i >= 0; i--)
-        {
-            Debug.Log(array[i]);
+            lastboss.Magic(5);
         }
     }
 
