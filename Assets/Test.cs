48edd6d4dd7a7e62b43d72d42683b37235294008
mamehata <117148042+mamehata@users.noreply.github.com>
2023-03-22 18:11:27 +0900

    Testの更新
    テックアカデミーカリキュラムLesson4課題「」

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -37,6 +37,18 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
         Boss lastboss = new Boss();
 
         lastboss.Attack();
